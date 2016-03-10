eb3f1beb1bc383644bea1fa84e2506fddaf50cd8
lechos <linneasaltel@gmail.com>
2016-03-10 03:01:54 +0000

    third commit

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -5,9 +5,19 @@ $(function() {
 
     $.get("/contacts", function(contacts) {
         contacts.forEach(function(contact) {
-          console.log(contact.first_name);
-          
+          $("#first_name").append(contact.first_name);
+          $("#last_name").append(contact.last_name);
+          $("#phone").append(contact.phone);
+          $("#email").append(contact.email);
+          //console.log(contact.first_name, contact.last_name, contact.phone, contact.email);
+    //     var body = $("#all_contacts").find('tbody');
+    //     var row = $('<tr>');
+    //     var column = $('<td>');
 
+    //     column.append(contact.first_name);
+    //     row.append(column);
+    //     body.append(row);
+      
       // data has the list of contacts
       // Iterate through contacts and append in a table of contacts
       });
